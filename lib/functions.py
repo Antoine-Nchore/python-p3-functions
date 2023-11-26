edce1d6ae1b68869234e01e58a32d2b5d6589a8a
nchore <nchoreanthony@gmail.com>
2023-11-26 10:54:31 +0300

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
-def greet(name):
-    pass
+def greet(name='Nchore'):
+    print(f"Hello {name}")
+greet()
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}")
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+add(20,30)
 
-def halve(number):
-    pass
+def halve(number=10):
+    division= number/2
+    print(division)
